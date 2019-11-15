07a13af6905545d02ca5145acfec9deb718b6c60
yasinallahdev <yasin.allahdev@gmail.com>
2019-11-15 12:33:17 -0500

    Update Project

--- app.js
+++ app.js
@@ -9,20 +9,13 @@ app.listen(8000, () => {
     console.log("Listening on port 8000");
 })
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html');
-})
-
-app.get('/css/style.css', (request, response) => {
-    response.sendFile(__dirname + '/css/style.css');
-})
-
-app.get('/js/script.js', (request, response) => {
-    response.sendFile(__dirname + '/js/script.js');
-})
-
-app.get('/img/south-coast-rail.jpg', (request, response) => {
-    response.sendFile(__dirname + '/img/south-coast-rail.jpg');
+    response.render('index.ejs');
 })
 
 app.post('/getInformation', (request, response) => {
