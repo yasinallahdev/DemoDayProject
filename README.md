aa4ae84594e820f349df8dfe5b4ee7b295c43ce7
yasinallahdev <yasin.allahdev@gmail.com>
2019-11-17 19:58:11 -0500

    Readme/Database

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# DemoDayProject
+# Accessible Trip Planner
+
+In a world where ADA-Accessibility is one of the most important things in the world, 
+
+# Installation
+1: Clone the repo
+2: run `npm install`
+
+# Running
+1: run `node app.js`
+2: navigate to `localhost:8000`
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://Frosty-Phoenix-Admin:BZBRbUBZBFLu0RLH@cluster0-urpux.azure.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demo'
+};
