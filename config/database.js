6b1a8fb7234cfd17f7fd78b45953ee10f138fe3a
Ya Sin Allah <yasin.allahdev@gmail.com>
2019-12-09 22:35:20 -0500

    Database Update

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 // config/database.js
 
+const USE_GLOBAL = true;
+
 function Database() {
 
     const password = "gaGZTFyWV3cULFef";
     this.dbName = 'demo';
-    this.url = `mongodb+srv://Frosty-Phoenix-Admin:${password}@cluster0-urpux.azure.mongodb.net/${this.dbName}?retryWrites=true&w=majority`;
+    this.url = (!USE_GLOBAL)?(`mongodb://localhost:27017/${this.dbName}`):(`mongodb+srv://Frosty-Phoenix-Admin:${password}@cluster0-urpux.azure.mongodb.net/${this.dbName}?retryWrites=true&w=majority`);
 
 }
 
